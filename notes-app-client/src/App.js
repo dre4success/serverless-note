0af79189306229de04a22452576aff1ea40e5cd1
Damilare <dre4success@live.com>
2017-11-28 01:10:27 +0100

    adding a deploy command to npm

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
           <Navbar fluid collapseOnSelect>
             <Navbar.Header>
               <Navbar.Brand>
-                <Link to="/">Scratch</Link>
+                <Link to="/">Noterer</Link>
               </Navbar.Brand>
               <Navbar.Toggle />
             </Navbar.Header>
